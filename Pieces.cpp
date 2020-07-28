fb84965332ab11d2983c16e11d5204d047b720c5
pora <porapat.ongkan@gmail.com>
2020-07-28 17:15:34 +0900

    UPDATE: piece info

--- Pieces.cpp
+++ Pieces.cpp
@@ -1,9 +1,9 @@
 #include <iostream>
 #include <vector>
 #include <fstream>
+#include <SFML/Graphics.hpp>
 
-#define PIECES_ROTATION 4
-#define BLOCK_SIZE 5
+#include "GameEnv.cpp"
 using namespace std;
 
 struct Loc
@@ -16,17 +16,21 @@ struct Loc
 
 class Pieces
 {
+private:
     vector<vector<vector<int>>> myBlock;
     vector<struct Loc> initLoc;
     int pieceType;
 
-private:
     void LoadPieceInfo();
     string GetPieceName();
 
 public:
-    int GetBlockType(x, y);
     Pieces(int _pieceType);
+
+    int GetBlockType(int x, int y, int rotation);
+    int GetXInitialPosition(int rotation);
+    int GetYInitialPosition(int rotation);
+    sf::Color GetPieceColor();
 };
 
 Pieces::Pieces(int _pieceType)
@@ -66,30 +70,64 @@ void Pieces::LoadPieceInfo()
     int x, y;
     for (int i = 0; i < PIECES_ROTATION; i++)
     {
-        char fileName = (char)'0' + i;
+        char fileName = (char) '0'+i;
         ifstream pieceFile(pieceFolder + fileName);
         if (pieceFile.is_open())
         {
             int j{0};
-            while (j < BLOCK_SIZE && getline(pieceFile, line))
+            while (j < PIECE_BLOCKS && getline(pieceFile, line))
             {
-                for (int k = 0; k < BLOCK_SIZE; k++)
-                    myBlock[i][j][k] = (int)line[k] - '0';
+                for (int k = 0; k < PIECE_BLOCKS; k++){
+                    myBlock[i][j][k] = (int)line[k]-'0';
+                }
 
                 j++;
             }
             pieceFile >> x >> y;
-            initLoc[i] = Loc(x, y);
+            initLoc.push_back(Loc(x, y));
             pieceFile.close();
         }
         else
         {
-            cout << pieceFolder + fileName;
+            cout << pieceFolder + fileName + " Not Found";
         }
     }
 }
 
-int Piece::GetBlockType(int x, int y, int rotation)
+sf::Color Pieces::GetPieceColor()
 {
-    return myBlock[rotation][x][y]
+    switch (pieceType)
+    {
+    case 0:
+        return sf::Color::Red;
+    case 1:
+        return sf::Color::Blue;
+    case 2:
+        return sf::Color::Green;
+    case 3:
+        return sf::Color::Yellow;
+    case 4:
+        return sf::Color::Magenta;
+    case 5:
+        return sf::Color::Cyan;
+    case 6:
+        return sf::Color(123, 23, 10);
+    default:
+        return sf::Color::Red;
+    }
+}
+
+int Pieces::GetBlockType(int x, int y, int rotation)
+{
+    return myBlock[rotation][x][y];
+}
+
+int Pieces::GetXInitialPosition(int rotation)
+{
+    return initLoc[rotation].x;
+}
+
+int Pieces::GetYInitialPosition(int rotation)
+{
+    return initLoc[rotation].y;
 }
